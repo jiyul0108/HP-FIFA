fc1eccd7109a0fab8033742d1880005e2b590abc
jiyul0108 <143976150+jiyul0108@users.noreply.github.com>
2023-10-11 17:56:51 +0900

    Add files via upload

---
+++ 홈페이지/main1.js
@@ -0,0 +1,13 @@
+const api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjE2NjE2NDE4ODciLCJhdXRoX2lkIjoiMiIsImV4cCI6MTcxMjU2NDU5NiwiaWF0IjoxNjk3MDEyNTk2LCJuYmYiOjE2OTcwMTI1OTYsInNlcnZpY2VfaWQiOiI0MzAwMTE0ODEiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.uZHyGkNLHrjgRsTZmOqAOgZ9mEtYEPyL8OUkBXIKrAg'; // 여기에 실제 API 키를 넣어주세요
+const nickname = '손흑민우';
+
+const url = `https://public.api.nexon.com/openapi/fconline/v1.0/users?nickname=${nickname}`;
+
+fetch(url, {
+  headers: {'Authorization': api_key}
+})
+.then(response => response.json())
+.then(data => {
+    var name = data.level;
+    document.querySelector(".fcName").innerHTML = name;
+});
