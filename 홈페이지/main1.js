91d9d6408283b093b2fb38427660ab5b4a74981c
jiyul0108 <143976150+jiyul0108@users.noreply.github.com>
2023-10-16 17:18:05 +0900

    2023-10-16 17:18

--- 홈페이지/main1.js
+++ 홈페이지/main1.js
@@ -1,11 +1,9 @@
-const api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjE2NjE2NDE4ODciLCJhdXRoX2lkIjoiMiIsImV4cCI6MTcxMjU2NDU5NiwiaWF0IjoxNjk3MDEyNTk2LCJuYmYiOjE2OTcwMTI1OTYsInNlcnZpY2VfaWQiOiI0MzAwMTE0ODEiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.uZHyGkNLHrjgRsTZmOqAOgZ9mEtYEPyL8OUkBXIKrAg'; // 여기에 실제 API 키를 넣어주세요
+const api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjE2NjE2NDE4ODciLCJhdXRoX2lkIjoiMiIsImV4cCI6MTcxMjU2NDU5NiwiaWF0IjoxNjk3MDEyNTk2LCJuYmYiOjE2OTcwMTI1OTYsInNlcnZpY2VfaWQiOiI0MzAwMTE0ODEiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.uZHyGkNLHrjgRsTZmOqAOgZ9mEtYEPyL8OUkBXIKrAg'; 
 const nickname = '호날두';
 const accessid = '41a65a008a7ec275fe382ee6';
 
 const url1 = `https://public.api.nexon.com/openapi/fconline/v1.0/users?nickname=${nickname}`;
-const url2 = `https://public.api.nexon.com/openapi/fconline/v1.0/users/${accessid}/markets?tradetype=buy&offset=0&limit=5`;
 
-const url3 = `https://public.api.nexon.com/openapi/fconline/v1.0/users/${accessid}`
 
 
 
@@ -22,20 +20,47 @@ fetch(url1,{headers: {'Authorization': api_key}})
     document.querySelector(".fcaccessId").innerHTML = accessId;
 });
 
-fetch(url2,{headers: {'Authorization': api_key}})
-.then(response => response.json())
-.then(data => {
-    var tradeDate = data.tradeDate;
-    document.querySelector(".fctradeDate").innerHTML = tradeDate;
+const tradetype = 'buy'; // 거래 유형 ('buy' 또는 'sell')
+const offset = 0; // 시작 위치 (기본값: 0)
+const limit = 10; // 가져올 개수 (기본값: 10)
+
+// 이전 코드는 여기에 그대로 둡니다...
+const tradeHistoryElement = document.getElementById('tradeHistory');
+
+const url2 = `https://public.api.nexon.com/openapi/fconline/v1.0/users/${accessid}/markets?tradetype=${tradetype}&offset=${offset}&limit=${limit}`;
+
+fetch(url2, { headers: { 'Authorization': api_key } })
+    .then(response => response.json())
+    .then(data => {
+        if (data.length > 0) {
+            data.forEach(trade => {
+                // 각 거래의 정보에 접근합니다.
+                const tradeDate = trade.tradeDate;
+                const saleSn = trade.saleSn;
+                const spid = trade.spid;
+                const grade = trade.grade;
+                const value = trade.value;
+
+                // 이 정보를 활용하여 HTML 요소를 생성하거나 업데이트합니다.
+                const tradeElement = document.createElement('div');
+                tradeElement.classList.add('trade');
+
+                tradeElement.innerHTML = `
+                    <p>거래일자: ${tradeDate}</p>
+                    <p>거래 고유 식별자: ${saleSn}</p>
+                    <p>선수 고유 식별자: ${spid}</p>
+                    <p>거래 선수 강화 등급: ${grade}</p>
+                    <p>거래 선수 가치(BP): ${value}</p>
+                `;
+
+                // tradeHistoryElement에 tradeElement를 추가합니다.
+                tradeHistoryElement.appendChild(tradeElement);
+            });
+        } else {
+            console.error('데이터가 비어있습니다.');
+        }
+    })
+    .catch(error => console.error('Error:', error));
 
 
-});
 
-fetch(url3,{headers: {'Authorization': api_key}})
-.then(response => response.json())
-.then(data => {
-    var name = data.nickname;
-    document.querySelector(".fcnickname").innerHTML = name;
-
-
-});
---
+++ 홈페이지/main2.js
@@ -0,0 +1,139 @@
+const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjE2NjE2NDE4ODciLCJhdXRoX2lkIjoiMiIsImV4cCI6MTcxMjU2NDU5NiwiaWF0IjoxNjk3MDEyNTk2LCJuYmYiOjE2OTcwMTI1OTYsInNlcnZpY2VfaWQiOiI0MzAwMTE0ODEiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.uZHyGkNLHrjgRsTZmOqAOgZ9mEtYEPyL8OUkBXIKrAg';  // 여러분의 API 키
+const tradetype = 'buy'; // 거래 유형 ('buy' 또는 'sell')
+const limit = 10; // 가져올 개수 (기본값: 10)
+
+// (1) 선수 고유식별자 metadata 불러오기
+const spidMetadataUrl = 'https://static.api.nexon.co.kr/fconline/latest/spid.json';
+
+fetch(spidMetadataUrl, { headers: { 'Authorization': apiKey } })
+    .then(response => response.json())
+    .then(spidData => {
+        // (2) 선수 시즌 metadata 불러오기
+        const seasonMetadataUrl = 'https://static.api.nexon.co.kr/fconline/latest/seasonid.json';
+
+        fetch(seasonMetadataUrl, { headers: { 'Authorization': apiKey } })
+            .then(response => response.json())
+            .then(seasonData => {
+                // (3) 선수 고유 식별 정보를 통해 선수 시즌 & 이름 가져오는 함수
+                function getPlayerInfo(playerID) {
+                    const playerInfo = [];
+
+                    const playerSeason = parseInt(String(playerID).slice(0, 3));
+
+                    console.log('spidData', spidData);
+                    console.log('seasonData', seasonData);
+
+                    for (const info of seasonData) {
+                        if (info.seasonId === playerSeason) {
+                            playerInfo.push(info.className);
+                        }
+                    }
+
+                    for (const info of spidData) {
+                        if (info.id === playerID) {
+                            playerInfo.push(info.name);
+                        }
+                    }
+
+                    return playerInfo.length === 2 ? playerInfo : ['Unknown', 'Unknown'];
+                }
+
+
+
+
+
+
+                // (4) 닉네임을 입력받아 유저 고유 식별자 가져오기
+                function getUserId(nickname) {
+                    const url = `https://public.api.nexon.com/openapi/fconline/v1.0/users?nickname=${nickname}`;
+                                
+                    return fetch(url, { headers: { 'Authorization': apiKey } })
+                        .then(response => response.json())
+                        .then(userInfo => userInfo.accessId);
+                }
+
+                // (5) 유저 고유 식별자를 이용하여 이적 시장 거래내역 불러오기
+                function getMarketInfo(userAccessId, number) {
+                    const url = `https://public.api.nexon.com/openapi/fconline/v1.0/users/${userAccessId}/markets?tradetype=${tradetype}&offset=0&limit=${number}`;
+
+                    return fetch(url, { headers: { 'Authorization': apiKey } })
+                        .then(response => response.json())
+                        .then(transInfo => {
+                            const trans = [];
+
+                            for (const info of transInfo) {
+                                const playerInfo = getPlayerInfo(info.spid);
+                                trans.push({
+                                    "구매 날짜": info.tradeDate.replace("T", " "),
+                                    "구매 선수": `${playerInfo[0]} ${playerInfo[1]}`,
+                                    "강화 단계": info.grade,
+                                    "가격": new Intl.NumberFormat().format(info.value)
+                                });
+                            }
+
+                            return trans;
+                        });
+                }
+
+                // (6) 깔끔한 정보 출력을 위한 함수 생성
+                function goodPrint(dictList) {
+                    let count = 1;
+
+                    for (const index of dictList) {
+                        console.log(count);
+
+                        for (const [k, v] of Object.entries(index)) {
+                            console.log(`${k}: ${v}\n`);
+                        }
+
+                        count++;
+                    }
+                }
+
+                // (7) HTML 페이지에 결과를 출력하는 함수
+                function displayOnHTML(result) {
+                    const outputElement = document.getElementById('output');
+                    outputElement.innerHTML = '';
+
+                    for (const info of result) {
+                        const playerInfo = getPlayerInfo(info["구매 선수"]);
+                        info["구매 선수"] = `${playerInfo[0]} ${playerInfo[1]}`;
+
+                        const transactionDiv = document.createElement('div');
+
+                        for (const [k, v] of Object.entries(info)) {
+                            const pElement = document.createElement('p');
+                            pElement.textContent = `${k}: ${v}`;
+                            transactionDiv.appendChild(pElement);
+                        }
+
+                        outputElement.appendChild(transactionDiv);
+                    }
+                }
+
+                // (8) 사용자 입력을 받아 처리하고 HTML에 출력하는 함수
+                function loadMarketInfo() {
+                    const nickname = document.getElementById('nickname').value;
+                    const number = document.getElementById('number').value;
+
+                    getUserId(nickname)
+                        .then(userAccessId => getMarketInfo(userAccessId, number))
+                        .then(transInfo => {
+                            for (const info of transInfo) {
+                                const playerInfo = getPlayerInfo(info["구매 선수"]);
+                                info["구매 선수"] = `${playerInfo[0]} ${playerInfo[1]}`;
+                            }
+
+                            displayOnHTML(transInfo);
+                        })
+                        .catch(error => console.error('Error:', error));
+                }
+
+                // (9) 조회하기 버튼 클릭 시 loadMarketInfo 함수 호출
+                document.getElementById('searchButton').addEventListener('click', loadMarketInfo);
+            })
+            .catch(error => console.error('Error:', error));
+    })
+    .catch(error => console.error('Error:', error));
+
+
