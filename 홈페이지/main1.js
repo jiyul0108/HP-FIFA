17e929a0e844df105eae2e988e405d691fa3e6d5
jiyul0108 <143976150+jiyul0108@users.noreply.github.com>
2023-10-16 10:08:39 +0900

    2023-10-16 10:08

--- 홈페이지/main1.js
+++ 홈페이지/main1.js
@@ -1,6 +1,6 @@
 const api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjE2NjE2NDE4ODciLCJhdXRoX2lkIjoiMiIsImV4cCI6MTcxMjU2NDU5NiwiaWF0IjoxNjk3MDEyNTk2LCJuYmYiOjE2OTcwMTI1OTYsInNlcnZpY2VfaWQiOiI0MzAwMTE0ODEiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.uZHyGkNLHrjgRsTZmOqAOgZ9mEtYEPyL8OUkBXIKrAg'; // 여기에 실제 API 키를 넣어주세요
-const nickname = '손흑민우';
-const accessid = '5b81e1decbe8609d95a1d6f3';
+const nickname = '호날두';
+const accessid = '41a65a008a7ec275fe382ee6';
 
 const url1 = `https://public.api.nexon.com/openapi/fconline/v1.0/users?nickname=${nickname}`;
 const url2 = `https://public.api.nexon.com/openapi/fconline/v1.0/users/${accessid}/markets?tradetype=buy&offset=0&limit=5`;
